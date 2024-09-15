e5792d8548e89716545fa9d83f3a144d2861f107
Asif <asifraza7410@gmail.com>
2024-09-15 18:57:21 +0530

    prime_number

---
+++ CheckPrimeWithinRange.py
@@ -0,0 +1,15 @@
+def prime(x,y):
+    mylist=[]
+    for i in range(x,y+1):
+        if i==0 or i==1:
+                continue
+        for j in range(2,int(i/2)+1):
+            
+            if(i%j == 0):
+                break
+        else:
+            mylist.append(i)
+    
+    return mylist
+l=prime(3,9)
+print(l)
